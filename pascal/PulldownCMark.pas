487758199d284b7762b5c7f9718f7d95babee052
vhanla <vhanla@gmail.com>
2024-07-18 19:24:01 -0500

    📦 Add Delphi Pascal Library that uses this rust dll

---
+++ pascal/PulldownCMark.pas
@@ -0,0 +1,69 @@
+unit PulldownCMark;
+
+interface
+
+{ Pulldown-Mark library}
+const
+  PULLDOWNCMARKDLL = 'pulldowncmarklib.dll';
+type
+// https://github.com/pulldown-cmark/pulldown-cmark/blob/8b060d5befc253914199d73c301283654ae0ca85/pulldown-cmark/src/lib.rs#L366
+  TPulldownCMarkOption = (
+    moEnableTables,
+    moEnableFootnotes,
+    moEnableStrikethrough,
+    moEnableTasklists,
+    moEnableSmartPunctuation,
+    moEnableHeadingAttributes,
+    moEnableYamlStyleMetadataBlocks,
+    moEnablePlusesDelimitedMetadataBlocks,
+    moEnableOldFootnotes,
+    moEnableMath,
+    moEnableGFM
+  );
+  TPulldownCMarkOptions = set of TPulldownCMarkOption;
+  TPulldownCMarkHelper = record
+  public
+    class function OptionsToU32(const Options: TPulldownCMarkOptions): UInt32; static;
+  end;
+
+  function strtomarkdown(input: PAnsiChar; options: uint32): PAnsiChar; cdecl;
+    external PULLDOWNCMARKDLL;
+  procedure free_string(ptr: PAnsiChar); cdecl;
+    external PULLDOWNCMARKDLL;
+
+  function StringToMarkdown(const Input: PAnsiChar; const Options: TPulldownCMarkOptions): PAnsiChar; cdecl;
+implementation
+
+{ TPulldownMarkHelper }
+
+class function TPulldownCMarkHelper.OptionsToU32(
+  const Options: TPulldownCMarkOptions): UInt32;
+const
+  OptionsFlags: array[TPulldownCMarkOption] of UInt32 = (
+    1 shl 1,
+    1 shl 2,
+    1 shl 3,
+    1 shl 4,
+    1 shl 5,
+    1 shl 6,
+    1 shl 7,
+    1 shl 8,
+    (1 shl 9) or (1 shl 2),
+    1 shl 10,
+    1 shl 11
+  );
+var
+  Option: TPulldownCMarkOption;
+begin
+  Result := 0;
+  for Option := Low(TPulldownCMarkOption) to High(TPulldownCMarkOption) do
+    if Option in Options then
+      Result := Result or OptionsFlags[Option];
+end;
+
+function StringToMarkdown(const Input: PAnsiChar; const Options: TPulldownCMarkOptions): PAnsiChar; cdecl;
+begin
+  Result := strtomarkdown(Input, TPulldownCMarkHelper.OptionsToU32(Options));
+end;
+
+end.
