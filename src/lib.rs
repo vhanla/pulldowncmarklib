47641dc5c436a4a85a5f9d40170caeba30ea05d5
vhanla <vhanla@gmail.com>
2024-07-18 19:20:31 -0500

    ⚙️ Add Pulldown CMark options to pass the DLL to

--- src/lib.rs
+++ src/lib.rs
@@ -5,20 +5,22 @@ use std::ptr;
 use pulldown_cmark::{Event, Parser, Options};
 
 #[no_mangle]
-pub extern "C" fn invert_string(input_ptr: *const c_char) -> *mut c_char {
+pub extern "C" fn strtomarkdown(input_ptr: *const c_char, options_flags: u32) -> *mut c_char {
     let input_cstr = unsafe{CStr::from_ptr(input_ptr)};
 
-    let input_str = std::str::from_utf8(input_cstr.to_bytes()).unwrap();//input_cstr.to_string_lossy().into_owned();
-    // input_cstr.to_str() does similar too
-
-    //let inverted_str: String = input_str.chars().rev().collect();
-
-
-    //let output_cstr = CString::new(inverted_str).expect("CString::new failes");
+    let input_str = std::str::from_utf8(input_cstr.to_bytes()).unwrap();
     let mut html_output = String::new();
-    let mut options = Options::empty();
-    options.insert(Options::ENABLE_TASKLISTS);
-    options.insert(Options::ENABLE_GFM);
+    let mut options = Options::from_bits(options_flags).unwrap_or(Options::empty());
+    //options.insert(Options::ENABLE_TABLES);
+    //options.insert(Options::ENABLE_FOOTNOTES);
+    //options.insert(Options::ENABLE_STRIKETHROUGH);
+    //options.insert(Options::ENABLE_SMART_PUNCTUATION);
+    //options.insert(Options::ENABLE_HEADING_ATTRIBUTES);
+    //options.insert(Options::ENABLE_TASKLISTS);
+    //options.insert(Options::ENABLE_YAML_STYLE_METADATA_BLOCKS);
+    //options.insert(Options::ENABLE_PLUSES_DELIMITED_METADATA_BLOCKS);
+    //options.insert(Options::ENABLE_MATH);
+    //options.insert(Options::ENABLE_GFM);
     let parser = pulldown_cmark::Parser::new_ext(&input_str, options);
     pulldown_cmark::html::push_html(&mut html_output, parser);
 
